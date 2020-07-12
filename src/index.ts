4618de34ddfd4bef5723fa479e3c6f87e3bd9cec
Michael Trinkies <mickyt@online.de>
2020-07-12 19:25:40 +0200

    changed sensor updates & added sensor prefix

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import { AccessoryPlugin, API, HAP, Logging, PlatformConfig, StaticPlatformPlugi
 import { TemperatureSensor } from "./TemperatureAccessory";
 import { SensorItem } from "./SensorItem";
 let request = require('request');
+let AsyncLock = require('async-lock');
+let Guid = require('guid');
 
 const PLATFORM_NAME = "GlancesTemperature";
 
 let hap: HAP;
+let lock = new AsyncLock({ timeout: 5000, maxPending: 10 });
 
 export = (api: API) => {
   hap = api.hap;
-
   api.registerPlatform(PLATFORM_NAME, ExampleStaticPlatform);
 };
 
@@ -18,24 +20,32 @@ class ExampleStaticPlatform implements StaticPlatformPlugin {
   private readonly log: Logging;
   private readonly hostname: String;
   private readonly port: Number;
+  private readonly updateInterval: number;
+  private readonly prefix: string;
   private foundAccessories: TemperatureSensor[] = [];
 
   constructor(log: Logging, config: PlatformConfig, api: API) {
     this.log = log;
     this.hostname = config.hostname;
-    this.port = config.port;
+    this.port = config.port ?? 61208;
+    this.updateInterval = config.updateInterval ?? 5000;
+    this.prefix = config.prefix ?? "";
     log.info(PLATFORM_NAME + " finished initializing!");
   }
 
   accessories(callback: (foundAccessories: AccessoryPlugin[]) => void): void {
 
     let _self = this;
+
+    setInterval(function () {
+      _self.updateAccessoires();
+    }, _self.updateInterval);
+
     this.addAccessoires(function (accessoires) {
-      // setTimeout(function () {
-      //   _self.updateAccessoires();
-      // }, 5000);
       callback(accessoires);
+      _self.log("found: '" + accessoires.length + "' accessoires");
     });
+
 
   }
 
@@ -45,49 +55,50 @@ class ExampleStaticPlatform implements StaticPlatformPlugin {
       for (let i = 0; i < sensors.length; i++) {
         let item = sensors[i] as SensorItem;
         if (item.type == "temperature_core") {
-          var accessory = new TemperatureSensor(hap, _self.log, item.label, item.value.toString());
-          _self.log("addedd: " + item.label);
+          var name = (_self.prefix + " " + item.label);
+          var accessory = new TemperatureSensor(hap, _self.log, name , item.value.toString());
           _self.foundAccessories.push(accessory);
         }
       }
+      callback(_self.foundAccessories);
     });
   }
 
   getSensors(callback: (accessoires: SensorItem[]) => void): void {
+    let _self = this;
+    let req = request('http://' + this.hostname + ':' + this.port + '/api/3/sensors', { json: true }, function (error: any, response: any, body: any) {
+      if (error) {
+        _self.log("request error: " + error);
+      }
 
-    try {
-      let _self = this;
-      let req = request('http://' + this.hostname + ':' + this.port + '/api/3/sensors', { json: true }, function (error: any, response: any, body: any) {
-        if (error) {
-          _self.log("request error: " + error);
-        }
+      let sensors = response.body as SensorItem[];
+      callback(sensors);
+    });
 
-        let sensors = response.body as SensorItem[];
-        callback(sensors);
-      });
-    }
-    catch (e) {
-      this.log("error: '" + e + "'");
-    }
   }
 
   updateAccessoires(): void {
     let _self = this;
-    this.getSensors(function (sensors) {
-      for (let i = 0; i < sensors.length; i++) {
-        var sensor = sensors[i];
-        if (sensor.type == "temperature_core") {
+    let key = Guid.create().value;
+    lock.acquire(key, function (done: any) {
+      _self.getSensors(function (sensors) {
+        for (let i = 0; i < sensors.length; i++) {
+          var sensor = sensors[i];
+          if (sensor.type == "temperature_core") {
 
-          var existendItem = _self.foundAccessories.find(a => (a as TemperatureSensor).name == sensor.label);
+            var existendItem = _self.foundAccessories.find(a => (a as TemperatureSensor).name == (_self.prefix + " " + sensor.label));
 
-          if (existendItem) {
-            _self.log("updating: " + sensor.label);
-            existendItem?.TempService.getCharacteristic(hap.Characteristic.CurrentTemperature).updateValue(sensor.value);
+            if (existendItem) {
+              existendItem?.TempService.getCharacteristic(hap.Characteristic.CurrentTemperature).updateValue(sensor.value);
+            }
           }
         }
-      }
+      });
+
+      done(null, null);
+    }, function (err: any, ret: any) {
     });
-  }
+  };
 }
 
 
