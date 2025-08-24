6e15b32d3a5eba5a868afc07bb4286ca9c501fab
Tim Potter <tim@timpotter.name>
2025-08-24 15:44:53 -0500

    update to current template, fix build errors

--- src/Handler/SensorsHandler.ts
+++ src/Handler/SensorsHandler.ts
@@ -1,75 +1,75 @@
 import {
-    Logging,
-    API,
-    HAP,
-} from "homebridge";
-import { HandlerBase } from "./HandlerBase";
-import { BaseAccessory } from "../Accessoires/BaseAccessory";
-import { SensorInfo } from "../Models/SensorInfo"
-import { TemperatureAccessory } from "../Accessoires/TemperatureAccessory"
-const got = require('got');
+  Logging,
+  API,
+  HAP,
+} from 'homebridge';
+import { HandlerBase } from './HandlerBase.js';
+import { BaseAccessory } from '../Accessoires/BaseAccessory.js';
+import { SensorInfo } from '../Models/SensorInfo.js';
+import { TemperatureAccessory } from '../Accessoires/TemperatureAccessory.js';
+import got from 'got';
 
 export class SensorsHandler extends HandlerBase {
-    sensors: TemperatureAccessory[];
-    constructor(hap: HAP,api: API, prefix: string, hostname: string, port: number, log: Logging) {
-        super(hap,api, prefix, hostname, port, log);
-        this.sensors = [];
+  sensors: TemperatureAccessory[];
+  constructor(hap: HAP,api: API, prefix: string, hostname: string, port: number, log: Logging) {
+    super(hap,api, prefix, hostname, port, log);
+    this.sensors = [];
+  }
+
+  async getServices(): Promise<BaseAccessory[]> {
+    this.log.info('SensorsHandler: starting discovery');
+
+    const accessories: BaseAccessory[] = [];
+    const sensors = await this.getSensors() as SensorInfo[];
+
+    this.log.info('SensorsHandler: received (' + sensors?.length + '): \'' + JSON.stringify(sensors) + '\'');
+
+    for (let i = 0; i < sensors.length; i++) {
+      const item = sensors[i] as SensorInfo;
+      if (item.type === 'temperature_core') {
+        this.log.info('Adding: ' + this.prefix + ' ' + item.label);
+        const accessory = new TemperatureAccessory(this.hap, this.api, this.log, this.prefix + ' ' + item.label, item.value.toString());
+        this.sensors.push(accessory);
+        accessories.push(accessory);
+      }
     }
+    this.log.info('SensorsHandler: discovery finished');
+    return Promise.resolve(accessories);
+  }
 
-    async getServices(): Promise<BaseAccessory[]> {
-        this.log.info("SensorsHandler: starting discovery");
-        let sensors = await this.getSensors() as SensorInfo[];
-        let accessories: BaseAccessory[];
-        accessories = [];
+  private async getSensors(): Promise<SensorInfo[]> {
+    try {
+      const response = await got('http://' + this.hostname + ':' + this.port + '/api/4/sensors', { throwHttpErrors: false });
 
-        this.log.info("SensorsHandler: received (" + sensors?.length + "): '" + JSON.stringify(sensors) + "'");
-
-        for (let i = 0; i < sensors.length; i++) {
-            let item = sensors[i] as SensorInfo;
-            if (item.type == "temperature_core") {
-                this.log.info("Adding: " + this.prefix + " " + item.label);
-                var accessory = new TemperatureAccessory(this.hap, this.api, this.log, this.prefix + " " + item.label, item.value.toString());
-                this.sensors.push(accessory);
-                accessories.push(accessory);
-            }
+      if (response.statusCode === 200) {
+        return Promise.resolve(JSON.parse(response.body) as SensorInfo[]);
+      }
+      // Check for glances version 3 api
+      if (response.statusCode === 404) {
+        this.log.debug('SensorHandler: v4 endpoint not found, falling back to v3');
+        const responsev3 = await got('http://' + this.hostname + ':' + this.port + '/api/3/sensors');
+        if (responsev3.statusCode === 200) {
+          return Promise.resolve(JSON.parse(responsev3.body) as SensorInfo[]);
         }
-        this.log.info("SensorsHandler: discovery finished");
-        return Promise.resolve(accessories);
+      }            
+    } catch (error) {
+      this.log.error('SensorsHandler: Failed request: \'' + error + '\'');
     }
+    return Promise.resolve([]);
+  }
 
-    private async getSensors(): Promise<SensorInfo[]> {
-        try {
-            let response = await got('http://' + this.hostname + ':' + this.port + '/api/4/sensors', { throwHttpErrors: false });
+  async updateServices(): Promise<void> {
+    const sensors = await this.getSensors();
+    for (let i = 0; i < sensors.length; i++) {
+      const sensor = sensors[i];
+      if (sensor.type === 'temperature_core') {
 
-            if (response.statusCode == 200) {
-                return Promise.resolve(JSON.parse(response.body) as SensorInfo[])
-            }
-            // Check for glances version 3 api
-            if (response.statusCode == 404) {
-              this.log.debug("SensorHandler: v4 endpoint not found, falling back to v3");
-              let responsev3 = await got('http://' + this.hostname + ':' + this.port + '/api/3/sensors');
-              if (responsev3.statusCode === 200) {
-                return Promise.resolve(JSON.parse(responsev3.body) as SensorInfo[]);
-              }
-            }            
-        } catch (error) {
-            this.log.error("SensorsHandler: Failed request: '" + error + "'")
+        const existendItem = this.sensors.find(a => (a as TemperatureAccessory).Name === (this.prefix + ' ' + sensor.label));
+
+        if (existendItem) {
+          existendItem?.TempService.getCharacteristic(this.hap.Characteristic.CurrentTemperature).updateValue(sensor.value);
         }
-        return Promise.resolve([]);
+      }
     }
-
-    async updateServices(): Promise<void> {
-        let sensors = await this.getSensors();
-        for (let i = 0; i < sensors.length; i++) {
-            var sensor = sensors[i];
-            if (sensor.type == "temperature_core") {
-
-                var existendItem = this.sensors.find(a => (a as TemperatureAccessory).Name == (this.prefix + " " + sensor.label));
-
-                if (existendItem) {
-                    existendItem?.TempService.getCharacteristic(this.hap.Characteristic.CurrentTemperature).updateValue(sensor.value);
-                }
-            }
-        }
-    };
+  };
 }
